4fcbecd648b8055663e234075f377be5c256040f
SFK <salihfurkankara@gmail.com>
2025-05-22 14:58:19 +0200

    API endpoint to get a list of all items

---
+++ server/src/models/Item.js
@@ -0,0 +1,32 @@
+import mongoose, { Types } from "mongoose";
+
+const itemSchema = new mongoose.Schema({
+  ownerId: { type: Types.ObjectId, required: true, index: true },
+  title: { type: String, required: true, index: true },
+  category: {
+    type: String,
+    enum: ["Electronics", "Home Appliances", "Vehicles"],
+    required: true,
+    index: true,
+  }, // Temporary enumerated categories, subject to change
+  model: { type: String, default: "" },
+  condition: {
+    type: String,
+    enum: ["Excellent", "Good", "Fair"],
+    required: true,
+    index: true,
+  }, // Temporary enumerated conditions, subject to change
+  borrowDuration: { type: Number, required: true, index: true },
+  description: { type: String, required: true },
+  images: { type: [String], required: true }, // implicit default value of []
+  reviews: { type: [Object], required: true }, // implicit default value of []
+  availability: { type: Boolean, default: true },
+  borrowedCount: { type: Number, default: 0 },
+  value: { type: Number, required: true },
+  price: { type: Number, required: true, index: true },
+  createdAt: { type: Date, default: Date.now() },
+});
+
+const Item = mongoose.model("items", itemSchema);
+
+export default Item;
