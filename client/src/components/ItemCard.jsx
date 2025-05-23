def24a449288007ecc968dfeb3f4edb7bab0ecb3
Hossein <Hossein.kelisa@gmail.com>
2025-05-23 09:49:24 +0200

    changed item ID to Description in ItemCard

--- client/src/components/ItemCard.jsx
+++ client/src/components/ItemCard.jsx
@@ -4,7 +4,7 @@ const ItemCard = ({ item }) => {
   return (
     <div style={{ border: "1px solid black", padding: "1rem" }}>
       <h2>{item.name}</h2>
-      <p>Item ID: {item.description}</p>
+      <p>Description: {item.description}</p>
     </div>
   );
 };
