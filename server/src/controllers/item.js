40bbf4349bbc08ccebe0a4ab63dca926ffaee567
Salih Furkan Kara <salihfurkankara@gmail.com>
2025-05-24 16:24:59 +0200

    Updated item.js to fix incorrect parsing of query strings

--- server/src/controllers/item.js
+++ server/src/controllers/item.js
@@ -3,8 +3,8 @@ import { logError } from "../util/logging.js";
 
 export const getItems = async (req, res) => {
   try {
-    const limit = req.query?.limit || 10;
-    const page = req.query?.page || 1;
+    const limit = parseInt(req.query?.limit) || 10;
+    const page = parseInt(req.query?.page) || 1;
     const offset = (page - 1) * limit;
 
     const items = await Item.find()
