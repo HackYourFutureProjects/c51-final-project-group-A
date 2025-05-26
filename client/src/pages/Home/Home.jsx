4266429e5ccf21c678626316a47faf0411033810
SFK <salihfurkankara@gmail.com>
2025-05-26 12:35:45 +0200

    Removed unused components

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,6 +1,5 @@
 import TEST_ID from "./Home.testid";
-import { useState, useEffect } from "react";
-import { useLocation } from "react-router-dom";
+import { useState } from "react";
 import ResultPage from "../components/ResultPage";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
@@ -9,48 +8,7 @@ import wallpaper from "../../assets/wallpaper.jpg";
 import "../../styles/HomeStyle.css";
 
 const Home = () => {
-  const [currentPage, setCurrentPage] = useState(1);
-  const itemsPerPage = 5;
-
-  // Get the current URL location
-  // Create a URLSearchParams instance to read query parameters
-  //Get the "search" parameter from the URL or default to an empty string
-  // Initialize local state for search input
-  const location = useLocation();
-  const searchParams = new URLSearchParams(location.search);
-  const searchQuery = searchParams.get("search") || "";
-  const [searchItem, setSearchItem] = useState(searchQuery);
-
-  // Temporary mock data for testing UI before backend is ready.
-  const items = [
-    { id: 1, name: "item1", description: "item1" },
-    { id: 2, name: "item2", description: "item2" },
-    { id: 3, name: "item3", description: "item3" },
-    { id: 4, name: "item4", description: "item4" },
-    { id: 5, name: "item5", description: "item5" },
-    { id: 6, name: "item6", description: "item6" },
-    { id: 7, name: "item7", description: "item7" },
-    { id: 8, name: "item8", description: "item8" },
-    { id: 9, name: "item9", description: "item9" },
-    { id: 10, name: "item10", description: "item10" },
-  ];
-
-  // Filter items based on search input
-  const filteredItems = items.filter((item) =>
-    item.name.toLowerCase().includes(searchItem.toLowerCase()),
-  );
-
-  // Reset to page 1 when search changes
-  useEffect(() => {
-    setCurrentPage(1);
-  }, [searchItem]);
-
-  // Pagination logic
-  const totalPages = Math.ceil(filteredItems.length / itemsPerPage);
-  const indexOfLastItem = currentPage * itemsPerPage;
-  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-  const currentItems = filteredItems.slice(indexOfFirstItem, indexOfLastItem);
-  const pages = [...Array(totalPages).keys()].map((num) => num + 1);
+  const [searchItem, setSearchItem] = useState("");
 
   return (
     <div data-testid={TEST_ID.container}>
@@ -61,15 +19,7 @@ const Home = () => {
         </div>
       )}
       {searchItem === "" && <ItemSlider />}
-      {searchItem !== "" && (
-        <ResultPage
-          currentItems={currentItems}
-          currentPage={currentPage}
-          totalPages={totalPages}
-          setCurrentPage={setCurrentPage}
-          pages={pages}
-        />
-      )}
+      {searchItem !== "" && <ResultPage />}
       <Footer />
     </div>
   );
--- client/src/pages/components/ResultPage.jsx
+++ client/src/pages/components/ResultPage.jsx
@@ -1,23 +1,22 @@
-import { useLocation } from "react-router-dom";
-import PropTypes from "prop-types";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ItemCard from "../../components/ItemCard";
-import Pagination from "../../components/Pagination";
+// import Pagination from "../../components/Pagination";
 import ViewToggle from "../../components/ViewToggle";
 import "../../styles/ResultPageStyle.css";
+import useFetch from "../../hooks/useFetch";
 
 const VIEW_MODES = { GRID: "grid", LINE: "line" };
 
-const ResultPage = ({
-  currentItems,
-  currentPage,
-  totalPages,
-  setCurrentPage,
-  pages,
-}) => {
-  const location = useLocation();
-  const searchParams = new URLSearchParams(location.search);
-  const searchQuery = searchParams.get("search") || "";
+const ResultPage = () => {
+  const [url, setUrl] = useState("/items");
+  const [items, setItems] = useState(null);
+  const [pagination, setPagination] = useState(null);
+  // const [currentPage, setCurrentPage] = useState(1);
+
+  const { performFetch, cancelFetch } = useFetch(url, (response) => {
+    setPagination(response.pagination);
+    setItems(response.result);
+  });
 
   const [viewMode, setViewMode] = useState(VIEW_MODES.LINE);
 
@@ -27,42 +26,44 @@ const ResultPage = ({
     );
   };
 
+  useEffect(() => {
+    const params = new URLSearchParams();
+    params.set("page", 1);
+    params.set("limit", 10);
+    params.set("sortBy", "createdAt");
+    params.set("sortOrder", "desc");
+    setUrl(`/items?${params.toString()}`);
+  }, []);
+
+  useEffect(() => {
+    performFetch();
+    return cancelFetch;
+  }, [url]);
+
   return (
     <div className="result-container">
       <ViewToggle viewMode={viewMode} toggleViewMode={toggleViewMode} />
-
-      {Array.isArray(currentItems) && currentItems.length > 0 ? (
+      {Array.isArray(items) && items.length > 0 ? (
         <div
           className={viewMode === VIEW_MODES.GRID ? "items-grid" : "items-list"}
         >
-          {currentItems.map((item) => (
-            <ItemCard key={item.id} item={item} />
+          {items.map((item) => (
+            <ItemCard key={item._id} item={item} />
           ))}
         </div>
       ) : (
-        <p style={{ textAlign: "center" }}>
-          No items found for <strong>{searchQuery}</strong>
-        </p>
+        <p style={{ textAlign: "center" }}>No items found...</p>
       )}
-
-      {Array.isArray(currentItems) && currentItems.length > 0 && (
+      {pagination && <p></p>}
+      {/* {Array.isArray(items) && items.length > 0 && (
         <Pagination
-          currentPage={currentPage}
-          totalPages={totalPages}
+          currentPage={pagination[0].currentPage}
+          totalPages={pagination[0].totalPages}
           setCurrentPage={setCurrentPage}
-          pages={pages}
         />
-      )}
+      )} */}
     </div>
   );
-};
-
-ResultPage.propTypes = {
-  currentItems: PropTypes.array.isRequired,
-  currentPage: PropTypes.number.isRequired,
-  totalPages: PropTypes.number.isRequired,
-  setCurrentPage: PropTypes.func.isRequired,
-  pages: PropTypes.array.isRequired,
 };
 
 export default ResultPage;
