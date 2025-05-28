ed4d35fff326584e9dc692127b75c25c0062698f
Samira <ahmadi.samira6761@gmail.com>
2025-05-28 15:25:00 +0200

    Build Item details page layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import Home from "./pages/Home/Home";
 import CreateUser from "./pages/User/CreateUser";
 import UserList from "./pages/User/UserList";
 import ResultPage from "./pages/ResultPage";
-
+import ItemDetailsPage from "./pages/ItemDetailsPage";
 const App = () => {
   return (
     <>
@@ -12,6 +12,7 @@ const App = () => {
         <Route path="/user" element={<UserList />} />
         <Route path="/user/create" element={<CreateUser />} />
         <Route path="/result" element={<ResultPage />} />
+        <Route path="/items/:id" element={<ItemDetailsPage />} />
       </Routes>
     </>
   );
