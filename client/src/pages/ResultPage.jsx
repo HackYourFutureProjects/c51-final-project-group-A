00d30f9e0343c6dbb4223ef1f42c87562e0927d2
SFK <salihfurkankara@gmail.com>
2025-05-29 11:11:13 +0200

    filters state

--- client/src/pages/ResultPage.jsx
+++ client/src/pages/ResultPage.jsx
@@ -5,38 +5,69 @@ import ViewToggle from "../components/ViewToggle";
 import "../styles/ResultPageStyle.css";
 import useFetch from "../hooks/useFetch";
 import Header from "../components/Header";
+import { useLocation } from "react-router-dom";
 
 const VIEW_MODES = { GRID: "grid", LINE: "line" };
 
 const ResultPage = () => {
   const [url, setUrl] = useState("/items");
   const [items, setItems] = useState(null);
+  const [filters, setFilters] = useState({
+    page: 1,
+    limit: 5,
+    sortBy: "createdAt",
+    sortOrder: "desc",
+  });
   const [pagination, setPagination] = useState(null);
-  const [currentPage, setCurrentPage] = useState(1);
   const [searchItem, setSearchItem] = useState("");
+  const [viewMode, setViewMode] = useState(VIEW_MODES.GRID);
+
+  const location = useLocation();
 
   const { performFetch, cancelFetch } = useFetch(url, (response) => {
     setPagination(response.pagination);
     setItems(response.result);
   });
 
-  const [viewMode, setViewMode] = useState(VIEW_MODES.GRID);
-
+  // Toggles between grid and line layout for items
   const toggleViewMode = () => {
     setViewMode((prev) =>
       prev === VIEW_MODES.GRID ? VIEW_MODES.LINE : VIEW_MODES.GRID,
     );
   };
 
+  // Handles state changes for current page
+  const handlePageChange = (newPage) => {
+    setFilters((prevFilters) => ({
+      ...prevFilters,
+      page: newPage,
+    }));
+  };
+
   useEffect(() => {
     const params = new URLSearchParams();
-    params.set("page", currentPage);
-    params.set("limit", 5);
-    params.set("sortBy", "createdAt");
-    params.set("sortOrder", "desc");
+
+    // Check if user navigated to the result page using the sidebar
+    const category = new URLSearchParams(location);
+    category.has("search") && params.set("category", category.get("category"));
+
+    // Set required string queries for use with useFetch
+    params.set("page", filters.page);
+    params.set("limit", filters.limit);
+    params.set("sortBy", filters.sortBy);
+    params.set("sortOrder", filters.sortOrder);
+
+    // Set optional string queries
+    filters.category && params.set("category", filters.category);
+    filters.condition && params.set("condition", filters.condition);
+    filters.availability && params.set("availability", filters.availability);
+    filters.minPrice && params.set("minPrice", filters.minPrice);
+    filters.maxPrice && params.set("maxPrice", filters.maxPrice);
+    filters.minDuration && params.set("minDuration", filters.minDuration);
+    filters.maxDuration && params.set("maxDuration", filters.maxDuration);
 
     setUrl(`/items?${params.toString()}`);
-  }, [currentPage]);
+  }, [filters]);
 
   useEffect(() => {
     performFetch();
@@ -60,9 +91,9 @@ const ResultPage = () => {
       )}
       {pagination && (
         <Pagination
-          currentPage={currentPage}
+          currentPage={filters.page}
           totalPages={pagination[0].totalPages}
-          setCurrentPage={setCurrentPage}
+          setCurrentPage={handlePageChange}
           pages={Array.from(
             { length: pagination[0].totalPages },
             (_, i) => i + 1,
