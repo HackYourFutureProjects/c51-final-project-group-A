e3e3143b45423dac20479f1d244dc260cfd2f5e6
Hossein <Hossein.kelisa@gmail.com>
2025-05-30 19:19:02 +0200

    Add API to get item details by ID with validation and error handling

--- server/src/routes/item.js
+++ server/src/routes/item.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { getItems } from "../controllers/item.js";
+import { getItems, getItemById } from "../controllers/item.js";
 
 const itemRouter = express.Router();
 
 itemRouter.get("/", getItems);
+itemRouter.get("/:id", getItemById);
 
 export default itemRouter;
