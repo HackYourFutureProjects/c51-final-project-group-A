a4a609943ec52da074c89a4e88b42efeac07f7f7
Hossein <Hossein.kelisa@gmail.com>
2025-05-30 21:27:10 +0200

    add API endpoint to get item images by item ID

--- server/src/controllers/item.js
+++ server/src/controllers/item.js
@@ -40,6 +40,27 @@ export const getItemById = async (req, res) => {
     return res.status(500).json({ success: false, msg: "Server error" }); // only shows generic message to user
   }
 };
+export const getItemImages = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res
+      .status(400)
+      .json({ success: false, msg: "Invalid item ID Format" });
+  }
+
+  try {
+    const item = await Item.findById(id).select("images");
+    if (!item) {
+      return res.status(404).json({ success: false, msg: "Item not found" });
+    }
+
+    return res.status(200).json({ success: true, result: item.images });
+  } catch (error) {
+    logError(error);
+    return res.status(500).json({ success: false, msg: "Server error" });
+  }
+};
 /*
   Returns an aggregation pipeline based on the request queries
   Queries that have fallback default values [
--- server/src/routes/item.js
+++ server/src/routes/item.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { getItems, getItemById } from "../controllers/item.js";
+import { getItems, getItemById, getItemImages } from "../controllers/item.js";
 
 const itemRouter = express.Router();
 
 itemRouter.get("/", getItems);
 itemRouter.get("/:id", getItemById);
+itemRouter.get("/:id/images", getItemImages);
 
 export default itemRouter;
