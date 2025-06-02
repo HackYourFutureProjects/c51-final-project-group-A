7b8c6e5c3ddaa35973bbac6f6002dee26108295c
SFK <salihfurkankara@gmail.com>
2025-06-02 11:07:14 +0200

    image added

--- client/src/components/ItemCard.jsx
+++ client/src/components/ItemCard.jsx
@@ -1,17 +1,18 @@
 import PropTypes from "prop-types";
 import "../styles/ItemCardStyle.css";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 // component to display a single item card on ResultPage and HomePage
 const ItemCard = ({ item }) => {
+  const navigate = useNavigate();
+
   return (
-    <div className="item-card">
-      <Link to={`/items/${item._id}`}>
-        <h3>{item.title}</h3>
-        <p>Model: {item.model}</p>
-        <p>Condition: {item.condition}</p>
-        <p>Price: {item.price}</p>
-      </Link>
+    <div className="item-card" onClick={() => navigate(`/items/${item._id}`)}>
+      <h3>{item.title}</h3>
+      <img src={item.images[0]} alt={item.title} />
+      <p>Model: {item.model}</p>
+      <p>Condition: {item.condition}</p>
+      <p>Price: {item.price}</p>
     </div>
   );
 };
@@ -20,6 +21,7 @@ const ItemCard = ({ item }) => {
 ItemCard.propTypes = {
   item: PropTypes.shape({
     _id: PropTypes.string.isRequired,
+    images: PropTypes.array.isRequired,
     title: PropTypes.string.isRequired,
     model: PropTypes.string.isRequired,
     condition: PropTypes.string.isRequired,
