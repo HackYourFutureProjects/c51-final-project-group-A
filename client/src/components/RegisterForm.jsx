db5fc54bf2f6e3c8fcb8c8bafbfcf1cd694db03a
Samira <ahmadi.samira6761@gmail.com>
2025-06-10 14:46:44 +0200

    Tyding up some feature in project

--- client/src/components/RegisterForm.jsx
+++ client/src/components/RegisterForm.jsx
@@ -4,25 +4,13 @@ export default function RegisterForm({ handleSubmit }) {
   return (
     <form className="auth-form" onSubmit={handleSubmit}>
       <label htmlFor="name">Name</label>
-      <input id="name" name="name" type="text" required placeholder="Name" />
+      <input id="name" name="name" type="text" required />
 
       <label htmlFor="email">Email</label>
-      <input
-        id="email"
-        name="email"
-        type="email"
-        required
-        placeholder="email"
-      />
+      <input id="email" name="email" type="email" required />
 
       <label htmlFor="password">Password</label>
-      <input
-        id="password"
-        name="password"
-        type="password"
-        required
-        placeholder="password"
-      />
+      <input id="password" name="password" type="password" required />
 
       <button type="submit">Sign up</button>
     </form>
