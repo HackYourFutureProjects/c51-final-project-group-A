94bc28269d023e441db2973b0b9e6d8895c8e1c6
Hossein <Hossein.kelisa@gmail.com>
2025-06-15 14:35:29 +0200

    improve authUser middleware and add GET  /me  route for user profile

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -12,5 +12,13 @@ const router = express.Router();
 router.post("/register", validateRegisterInput, registerUser);
 router.post("/login", validateLoginInput, loginUser);
 router.delete("/delete", validateDeleteRequest, authUser, deleteUser);
+router.get("/me", authUser, (req, res) => {
+  res.json({
+    id: req.user._id,
+    email: req.user.email,
+    name: req.user.name || "No name",
+    // we can add other user fields here if needed
+  });
+});
 
 export default router;
