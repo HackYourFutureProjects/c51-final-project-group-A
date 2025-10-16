1a6454f00bea37507305ada6a02ac56d75e5707e
Hossein <Hossein.kelisa@gmail.com>
2025-10-16 17:04:59 +0200

    update

---
+++ client/src/components/ExplainText2.jsx
@@ -0,0 +1,13 @@
+import "./ExplainText2.css";
+function ExplainText() {
+  return (
+    <div className="explain-text2">
+      <p>
+        Small acts of sharing create big change. Start today and make your
+        community a little brighter.
+      </p>
+    </div>
+  );
+}
+
+export default ExplainText;
