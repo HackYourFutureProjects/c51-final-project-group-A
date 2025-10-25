4fc76da82a30c29abfb8b6cebe87313414d54ca8
Hossein <Hossein.kelisa@gmail.com>
2025-10-25 19:41:52 +0200

    add new component to HomePage

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -7,6 +7,7 @@ import ExplainText2 from "../../components/HomeComponents/ExplainText2";
 import SharePrompt from "../../components/HomeComponents/SharePrompt";
 import Banner from "../../components/HomeComponents/Banner";
 import Shape from "../../components/HomeComponents/Shape";
+import HorizontalScrolling from "../../components/HomeComponents/HorizontalScrolling";
 
 const Home = () => {
   return (
@@ -18,6 +19,7 @@ const Home = () => {
       <ItemSlider />
       <Banner />
       <SharePrompt />
+      <HorizontalScrolling />
       <Footer />
     </div>
   );
