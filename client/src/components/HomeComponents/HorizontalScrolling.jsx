c8349954762c99382a2843d33f7f7866d156ca63
Hossein <Hossein.kelisa@gmail.com>
2025-10-25 19:40:16 +0200

    add new component

---
+++ client/src/components/HomeComponents/HorizontalScrolling.jsx
@@ -0,0 +1,8 @@
+function HorizontalScrolling() {
+  return (
+    <div className="horizontal-scrolling">
+      <p>Scroll horizontally to see more items!</p>
+    </div>
+  );
+}
+export default HorizontalScrolling;
