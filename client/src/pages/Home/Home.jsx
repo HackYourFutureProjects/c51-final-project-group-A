8c571a5f963b4456d5a20b23fc8dd51f73080fff
Hossein <Hossein.kelisa@gmail.com>
2025-10-25 19:46:22 +0200

    change priority of components in Homepage

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,14 +1,13 @@
 import TEST_ID from "./Home.testid";
 import Header from "../../components/Header";
-import Footer from "../../components/Footer";
-import ItemSlider from "../../components/HomeComponents/ItemSlider";
 import ExplainText from "../../components/HomeComponents/ExplainText";
 import ExplainText2 from "../../components/HomeComponents/ExplainText2";
-import SharePrompt from "../../components/HomeComponents/SharePrompt";
-import Banner from "../../components/HomeComponents/Banner";
 import Shape from "../../components/HomeComponents/Shape";
 import HorizontalScrolling from "../../components/HomeComponents/HorizontalScrolling";
-
+import ItemSlider from "../../components/HomeComponents/ItemSlider";
+import Banner from "../../components/HomeComponents/Banner";
+import SharePrompt from "../../components/HomeComponents/SharePrompt";
+import Footer from "../../components/Footer";
 const Home = () => {
   return (
     <div data-testid={TEST_ID.container}>
@@ -16,10 +15,11 @@ const Home = () => {
       <ExplainText />
       <ExplainText2 />
       <Shape />
+      <HorizontalScrolling />
+      <Shape />
       <ItemSlider />
       <Banner />
       <SharePrompt />
-      <HorizontalScrolling />
       <Footer />
     </div>
   );
